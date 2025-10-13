03deeefb4be8e7c329178c11167035fcf2b8611e
Ayda Afsharheravan <ayafshar@f0r6s6.codam.nl>
2025-10-13 16:45:03 +0200

    all

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: ayafshar <ayafshar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 13:10:57 by ayafshar          #+#    #+#             */
-/*   Updated: 2025/10/13 16:07:41 by ayafshar         ###   ########.fr       */
+/*   Updated: 2025/10/13 16:39:48 by ayafshar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count_words(const char *s, char c)
+static size_t	count_words(const char *s, char c)
 {
 	size_t	i;
 	size_t	count;
@@ -30,32 +30,49 @@ int	count_words(const char *s, char c)
 	return (count);
 }
 
-char	**ft_split(const char *s, char c)
+static void	free_all(char **arr, size_t j)
 {
+	while (j > 0)
+	{
+		free(arr[j--]);
+		free(arr);
+	}
+}
+
+static char	**fill_array(char **arr, char *s, char c)
+{
+	size_t	start;
 	size_t	i;
 	size_t	j;
-	size_t	start;
-	char	**arr;
 
+	start = 0;
 	i = 0;
 	j = 0;
-	start = 0;
-	arr = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!arr)
-		return (NULL);
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		while (s[i] && s[i] == c)
 			i++;
 		if (!s[i])
 			break ;
 		start = i;
 		while (s[i] != '\0' && s[i] != c)
 			i++;
-		len = i - start;
-		arr[j] = ft_substr(s, start, len);
+		arr[j] = ft_substr(s, start, i - start);
+		if (!arr[j])
+			return (free_all(arr, j));
 		j++;
 	}
 	arr[j] = NULL;
+	return (arr);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**arr;
+
+	arr = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	arr = fill_array(arr, s, c);
 	return (arr);
 }
